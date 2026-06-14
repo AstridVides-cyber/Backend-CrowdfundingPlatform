424edb08a21befdddae651f2dd5feb89d43f2020
JaimeArturoMaganaNerio <neriojaime9@gmail.com>
2026-06-14 12:05:49 -0600

    Feat:Agregando DTO , service , controller ,  repository

---
+++ CrowdfundingPlatform/src/main/java/com/example/crowdfundingplatform/service/CampaignService.java
@@ -0,0 +1,57 @@
+package com.example.crowdfundingplatform.service;
+
+import com.example.crowdfundingplatform.domain.entity.Campaign;
+import com.example.crowdfundingplatform.dto.CampaignDTO;
+import com.example.crowdfundingplatform.repository.CampaignRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CampaignService {
+
+    private final CampaignRepository campaignRepository;
+
+    public CampaignDTO createCampaign(CampaignDTO campaignDTO) {
+        // 1. Convertimos de DTO a Entidad
+        Campaign campaign = new Campaign();
+        campaign.setTitle(campaignDTO.getTitle());
+        campaign.setDescription(campaignDTO.getDescription());
+
+        campaign.setGoal(campaignDTO.getGoal());
+
+        Campaign savedCampaign = campaignRepository.save(campaign);
+
+        // 3. Devolvemos el DTO
+        return mapToDTO(savedCampaign);
+    }
+
+    public List<CampaignDTO> getAllCampaigns() {
+        List<Campaign> campaigns = campaignRepository.findAll();
+        return campaigns.stream()
+                .map(this::mapToDTO)
+                .collect(Collectors.toList());
+    }
+
+    public CampaignDTO getCampaignById(Long id) {
+        Campaign campaign = campaignRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Error: No se encontró la campaña con el ID: " + id));
+        return mapToDTO(campaign);
+    }
+
+    // Método auxiliar
+    private CampaignDTO mapToDTO(Campaign campaign) {
+        return CampaignDTO.builder()
+                .id(campaign.getId())
+                .title(campaign.getTitle())
+                .description(campaign.getDescription())
+                .goal(campaign.getGoal())
+                .currentAmount(BigDecimal.ZERO)//devolveremos cero mientras se programa la logica
+                .campaignStatus(campaign.getStatus() != null ? campaign.getStatus().name() : null)
+                .build();
+    }
+}
